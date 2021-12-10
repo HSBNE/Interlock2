d790031bdec8d749f087af444a0ac52ccfc7966a
Ryan Kirkpatrick <ryan.kptk@outlook.com>
2021-12-10 20:37:16 +1000

    Updated .gitignore

--- include/Secrets.h
+++ include/Secrets.h
@@ -2,7 +2,7 @@
 #define InterlockSecrets
 
 namespace Core {
-inline const char* wifiPassword = "";
+inline const char* wifiPassword = "junk";
 inline const char* otaPassword = "";
 inline const char* hostSecret = ""; // Secret to talk to the host on
 inline const long skeletonKeyCardNumber = 0; // This card number will allways grant access. (State = ACCESS_PULSE). Used to open doors in a worst case scenario.
